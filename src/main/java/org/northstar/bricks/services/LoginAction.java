a5ea050699fd8522f3ec8039a126db782c79f529
djunhao@home <cbwci@163.com>
2012-09-18 01:05:11 +0800

    login minore fixes

--- src/main/java/org/northstar/bricks/services/LoginAction.java
+++ src/main/java/org/northstar/bricks/services/LoginAction.java
@@ -22,12 +22,9 @@ public class LoginAction {
 
     private String name;
     private String password;
-    private String lastUrl;
 
     private CurrentUser login;
     private Authentication auth;
-    @Inject
-    private Request request;
 
     @Inject
     public LoginAction(CurrentUser login, Authentication auth) {
@@ -51,25 +48,17 @@ public class LoginAction {
         this.password = password;
     }
 
-    public String getLastUrl() {
-        return lastUrl;
-    }
-
-    public void setLastUrl(String lastUrl) {
-        this.lastUrl = lastUrl;
-    }
-
     @Post
     public Reply login() {
         System.out.println("loginAction excuted ...");
         login = auth.authenticate(name, password);
         Map<String, String> result = new HashMap<String, String>();
         if (login.isAuthenticated()) {
-            //return Reply.saying().redirect(lastUrl);
             result.put("info", "success");
         } else {
             result.put("info", "您输入的用户名或密码不正确！");
         }
         return Reply.with(result).as(Json.class);
+
     }
 }
